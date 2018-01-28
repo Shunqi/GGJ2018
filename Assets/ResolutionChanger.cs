16640e9410abe249fa17c326e89c3a933143616b
Hoi Man yuen <AomoriMayo@Hois-MacBook-Pro-2.local>
2018-01-28 11:54:54 +0800

    add resolution changes to previous fixes

---
+++ Assets/ResolutionChanger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResolutionChanger : MonoBehaviour {
+
+	int prevWidth;
+
+	// Use this for initialization
+	void Start () {
+		//Screen.SetResolution(768, 1024, false);
+		Screen.SetResolution(1536, 2048, false);
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		/*
+		if (prevWidth != Screen.width)
+		{
+			Screen.SetResolution(Screen.width, Screen.width / 3 * 4, false);
+			prevWidth = Screen.width;
+		}
+		*/
+	}
+}
